8f75d9ae5e98ff6b0e66bd1dfaef747053441d75
Ramon Smits <ramon.smits@gmail.com>
2015-10-10 16:18:28 +0200

    wip

---
+++ MsmqNativeStresstest/Class1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace MsmqNativeStresstest
+{
+    public class MessageProcessor//<TMessage>
+    {
+        private readonly bool Ambient;
+        private long counter;
+        private readonly MessageQueue[] Receivers;
+        private bool IsClosing;
+        //private PeekCompletedEventHandler x;
+        public MessageProcessor(string path)
+            : this(path, 1, false) { }
+        public MessageProcessor(string path, int count, bool ambient)
+            : base()
+        {
+            Ambient = ambient;
+
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentNullException("path");
+            if (!MessageQueue.Exists(path))
+                MessageQueue.Create(path, true);
+
+            this.Receivers = Enumerable.Range(0, (count <= 0) ? 1 : count)
+                .Select(i =>
+                {
+                    var queue = new MessageQueue(path, QueueAccessMode.Receive)
+                    {
+                        Formatter = new BinaryMessageFormatter()
+                    };
+                    queue.Purge();
+                    queue.MessageReadPropertyFilter.SetAll();
+                    return queue;
+                })
+                .ToArray();
+        }
+        public void Close()
+        {
+            this.IsClosing = true;
+            this.OnClosing();
+            foreach (var queue in Receivers)
+            {
+                if (Ambient)
+                    queue.PeekCompleted -= queue_PeekCompletedAmbient;
+                else
+                    queue.PeekCompleted -= queue_PeekCompleted;
+
+                queue.Close();
+            }
+            while (this.IsProcessing)
+                Thread.Sleep(100);
+            this.IsClosing = this.IsOpen = false;
+            this.OnClosed();
+        }
+        public bool IsOpen { get; private set; }
+        protected bool IsProcessing
+        {
+            get { return Interlocked.Read(ref counter) > 0; }
+        }
+        protected virtual void OnClosing() { }
+        protected virtual void OnClosed() { }
+        protected virtual void OnOpening() { }
+        protected virtual void OnOpened() { }
+        public void Open()
+        {
+            if (this.IsOpen)
+                throw new Exception("This processor is already open.");
+            this.OnOpening();
+            foreach (var queue in this.Receivers)
+            {
+                if (Ambient)
+                    queue.PeekCompleted += queue_PeekCompletedAmbient;
+                else
+                    queue.PeekCompleted += queue_PeekCompleted;
+
+                queue.BeginPeek();
+            }
+            this.IsOpen = true;
+            this.OnOpened();
+        }
+        //protected abstract void Process(TMessage @object);
+        private void Handle(Message message)
+        {
+            Trace.Assert(null != message);
+            Interlocked.Increment(ref counter);
+            try
+            {
+                Program.Signal();
+            }
+            finally
+            {
+                Interlocked.Decrement(ref counter);
+            }
+        }
+        private void queue_PeekCompleted(object sender, PeekCompletedEventArgs e)
+        {
+            var queue = (MessageQueue)sender;
+            var transaction = new MessageQueueTransaction();
+            transaction.Begin();
+            try
+            {
+                // if the queue closes after the transaction begins,
+                // but before the call to Receive, then an exception
+                // will be thrown and the transaction will be aborted
+                // leaving the message to be processed next time
+                var msg = queue.Receive(transaction);
+                this.Handle(msg);
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Abort();
+                Trace.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (!this.IsClosing)
+                    queue.BeginPeek();
+            }
+        }
+        private void queue_PeekCompletedAmbient(object sender, PeekCompletedEventArgs e)
+        {
+            var queue = (MessageQueue)sender;
+            using (var transaction = new TransactionScope())
+            {
+                try
+                {
+                    // if the queue closes after the transaction begins,
+                    // but before the call to Receive, then an exception
+                    // will be thrown and the transaction will be aborted
+                    // leaving the message to be processed next time
+                    var msg = queue.Receive(MessageQueueTransactionType.Automatic);
+                    Handle(msg);
+                    transaction.Complete();
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    if (!this.IsClosing)
+                        queue.BeginPeek();
+                }
+            }
+        }
+    }
+
+}
---
+++ MsmqNativeStresstest/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MsmqNativeStresstest
+{
+    class Program
+    {
+        private static readonly CountdownEvent batchCountdownEvent = new CountdownEvent(batchSize);
+        private readonly MessageQueue writeQueue = new MessageQueue(@".\private$\msmqnative", QueueAccessMode.Send);
+        private const int batchSize = 1000;
+        private long total;
+        private readonly byte[] head = new byte[1024];
+        private readonly byte[] body = new byte[1024];
+
+        static void Main()
+        {
+            Console.WriteLine("64bit: {0}", Environment.Is64BitProcess);
+            var reader = new MessageProcessor(@".\private$\msmqnative", 4, false);
+
+            reader.Open();
+            var p = new Program();
+            p.Init();
+            p.Loop();
+        }
+
+        private void Init()
+        {
+            if (!MessageQueue.Exists(@".\private$\msmqnative"))
+                MessageQueue.Create(@".\private$\msmqnative", true);
+        }
+
+        private void Loop()
+        {
+
+            //q2.Purge();
+            var start = Stopwatch.StartNew();
+            while (true)
+            {
+                batchCountdownEvent.Reset();
+                var sp = Stopwatch.StartNew();
+
+                Parallel.For(0, batchSize, i =>
+                {
+                    var current = Interlocked.Increment(ref total);
+                    var msg = new Message
+                    {
+                        Recoverable = true,
+                        Label = current.ToString(),
+                        Body = body,
+                        Extension = head,
+                    };
+                    writeQueue.Send(msg, MessageQueueTransactionType.Single);
+
+                });
+                batchCountdownEvent.Wait();
+                var elapsed = sp.Elapsed.TotalSeconds;
+                Console.WriteLine("{0:N0}msg/s ~{1:N0} +{2:N0} {3}s", batchSize / elapsed, total / start.Elapsed.TotalSeconds, total, start.Elapsed.TotalSeconds);
+            }
+        }
+
+        internal static void Signal()
+        {
+            batchCountdownEvent.Signal();
+        }
+    }
+}
